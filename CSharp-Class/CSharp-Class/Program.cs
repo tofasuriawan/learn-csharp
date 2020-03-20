f692ada3d9d541aa763887d59d55fc0ba1707da1
Tofa Suriawan <tofa.suriawan@gmail.com>
2020-03-20 15:54:49 +0700

    Add c# class tutorial

---
+++ CSharp-Class/CSharp-Class/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharp_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Tofa Suriawan", 1000000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} balance.");
+
+            account.MakeWithdrawal(500000, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100000, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -550000);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                account.MakeWithdrawal(750000, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
